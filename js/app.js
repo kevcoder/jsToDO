23e123fabcac9c3e9cf0f6a29095b785822a30f0
kevcoder <kevcodes@gmail.com>
2016-11-22 10:07:48 -0500

    initial js functionality

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var taskInput = document.getElementById("new-task");
+var addButotn = document.getElementsByTagName("button")[0];
+var incompleteTasksHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder = document.getElementById("completed-tasks");
+
+var addTask = function(){
+
+}
+
+var editTask = function(){
+
+}
+
+var deleteTask = function(){
+
+}
+
+var taskCompleted = function(){
+
+}
+
+var taskComplete = function(){
+
+}
