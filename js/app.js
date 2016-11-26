df0ff09ca0b9760e1faf455938b28da55a8f98d5
kevcoder <kevcodes@gmail.com>
2016-11-26 15:19:01 -0500

    js functionality updated

--- js/app.js
+++ js/app.js
@@ -3,8 +3,28 @@ var addButton = document.getElementsByTagName("button")[0];
 var incompleteTasksHolder = document.getElementById("incomplete-tasks");
 var completedTasksHolder = document.getElementById("completed-tasks");
 
+var createNewTaskElement = function(taskString){
+	var listItem = document.createElement("li");
+	var checkBox = document.createElement("input");
+	var label =	document.createElement("label");
+	var editInput = document.createElement("input");
+	var editButton = document.createElement("button");
+	var deleteButton = document.createElement("button");
+	
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(editButton);
+	listItem.appendChild(deleteButton);
+
+	return listItem;
+}
+
 var addTask = function(){
 	console.log("Add task...")
+	var listItem = createNewTaskElement("Some new Task");
+	incompleteTasksHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskCompleted);
 }
 
 var editTask = function(){
@@ -13,24 +33,36 @@ var editTask = function(){
 
 var deleteTask = function(){
 	console.log("delete task...")
+	var listItem = this.parentNode;
+	var ul = listItem.parentNode;
+
+	ul.removeChild(listItem);
 }
 
 var taskCompleted = function(){
-	console.log("task complete")
+	console.log("task complete");
+	var listItem = this.parentNode;
+	completedTasksHolder.appendChild(listItem);
 }
 
 var taskInComplete = function(){
 	console.log("Task incomplete task...")
+	var listItem = this.parentNode
+	incompleteTasksHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskInComplete)
 }
 
 var bindTaskEvents = function(taskListItem, checkBoxEventHandler){
 	console.log("bink list item event");
+	var checkBox = taskListItem.querySelector("input[type=checkbox]");
+	var editButton = taskListItem.querySelector("button.edit");
+	var deleteButton = taskListItem.querySelector("button.delete");
 
+	editButton.onclick = editTask;
 
+	deleteButton.onclick = deleteTask;
 
-
-
-
+	checkBox.onchange = checkBoxEventHandler; 
 }
 
 
